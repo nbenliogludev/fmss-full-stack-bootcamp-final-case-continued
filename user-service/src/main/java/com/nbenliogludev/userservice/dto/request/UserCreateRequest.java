b8a6839b71e7fa91e4ddbd56bb8cb0e83fd919ef
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2024-08-05 11:41:01 +0300

    Modify docker-compose.yml .

--- user-service/src/main/java/com/nbenliogludev/userservice/dto/request/UserCreateRequest.java
+++ user-service/src/main/java/com/nbenliogludev/userservice/dto/request/UserCreateRequest.java
@@ -2,11 +2,14 @@ package com.nbenliogludev.userservice.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
 
 /**
  * @author nbenliogludev
  */
+
+@Builder
 public record UserCreateRequest (
 
     @NotBlank(message = "Name is required.")
